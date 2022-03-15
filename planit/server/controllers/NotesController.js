cbf7e4f59145512a06bc7bf597534c78908f87fe
Katie McCauley <katiemccau02@gmail.com>
2022-03-14 21:44:51 -0600

    finsihed basic backend

--- planit/server/controllers/NotesController.js
+++ planit/server/controllers/NotesController.js
@@ -7,15 +7,37 @@ export class NotesController extends BaseController {
     super('api/projects/:projectId/notes')
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .get('', this.getAllNotes)
       .post('', this.createNote)
+      .delete('/:noteId', this.deleteNote)
 
   }
 
+
+  async getAllNotes(req, res, next) {
+    try {
+      const notes = await notesService.getAllNotes(req.query)
+      return res.send(notes)
+    } catch (error) {
+      next(error)
+    }
+  }
   async createNote(req, res, next) {
     try {
       req.body.creatorId = req.userInfo.id
       req.body.projectId = req.params.projectId
       const note = await notesService.createNote(req.body)
+      return res.send(note)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteNote(req, res, next) {
+    try {
+      const creatorId = req.userInfo.id
+      const noteId = req.params.noteId
+      const note = await notesService.deleteNote(noteId, creatorId)
       return res.send(note)
     } catch (error) {
       next(error)
--- planit/server/services/NotesService.js
+++ planit/server/services/NotesService.js
@@ -1,11 +1,28 @@
 import { dbContext } from "../db/DbContext"
+import { BadRequest } from "../utils/Errors"
 
 class NotesService {
+
+  async getAllNotes(query = {}) {
+    const notes = await dbContext.Notes.find(query).populate('creator')
+    return notes
+  }
+
   async createNote(body) {
     const note = await dbContext.Notes.create(body)
     await note.populate('creator')
     return note
   }
+
+  async deleteNote(noteId, creatorId) {
+    const original = await dbContext.Notes.findById(noteId).populate('creator')
+    if (original.creatorId.toString() !== creatorId) {
+      throw new BadRequest('can not delete the note because, it is not yours')
+    }
+    await original.remove()
+    return original
+  }
+
 
 }
 
